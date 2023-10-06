1f5d27e19c35a6b6fa2b13a736f94102e7984ded
Engedasew-Haile <engedasewhaile@gmail.com>
2023-10-06 14:22:17 +0000

    update

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,18 +1,5 @@
-"""export default function updateUniqueItems(map) {
-"""  if (!(map instanceof Map)) throw Error('Cannot process');
-"""  for (const i of map) if (i[1] === 1) map.set(i[0], 100);
-"""  return map;
-"""}
-const updateUniqueItems = (map) => {
-  if (map instanceof Map) {
-    map.forEach((value, key, map) => {
-      if (value === 1) {
-        map.set(key, 100);
-      }
-    });
-    return map;
-  }
-  throw new Error('Cannot process');
-};
-
-export default updateUniqueItems;
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) throw Error('Cannot process');
+  for (const i of map) if (i[1] === 1) map.set(i[0], 100);
+ return map;
+}
